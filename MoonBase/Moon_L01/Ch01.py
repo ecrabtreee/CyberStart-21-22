3af967690ca9271cabc40bd043aea079ce32fade
emi005 <95697775+emi005@users.noreply.github.com>
2022-02-17 08:28:37 -0500

    Update Ch01.py

--- MoonBase/Moon_L01/Ch01.py
+++ MoonBase/Moon_L01/Ch01.py
@@ -1,4 +1,5 @@
-# Briefing:
+# Briefing L01 C01
+# Programming 101
 # Good morning agent, welcome to your first day of training on the Moon Base. Our goal on this level is to teach you some programming basics. We use a programming language called Python at the CPA and today we're going to start with a simple task - teaching you a few simple things like code comments and using the print function.
 # We have an interactive code editor for you to learn on, the CPA MoonCoder. Simply change the code in the editor on the left side and click the green "Submit code" button to make it run. You'll then see the output of your code (along with a flag to submit if you get it correct) on the right side.
 # And don't forget, if you get stuck you can click "Use a hint". Whilst you're in training you won't lose any points for doing so.
