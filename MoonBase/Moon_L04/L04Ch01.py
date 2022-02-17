8b2a97d00a27d729973ecc610e624b3e60067220
emi005 <95697775+emi005@users.noreply.github.com>
2022-02-17 09:04:57 -0500

    Create L04Ch01.py

---
+++ MoonBase/Moon_L04/L04Ch01.py
@@ -0,0 +1,18 @@
+# Briefing L04 C01
+# Strange File
+# Ok, quick task for you agent - we've received a strange file from the first alien communication. It's at /tmp/alien-signal.txt, we need you to open and read the file.
+
+# Tip: Write a python script to open and read the file to get the flag.
+
+#-------------------------------------------------------------
+
+#
+# Ok, quick task for you agent - we've received a strange file from
+# the first alien communication.
+# It's at /tmp/alien-signal.txt, we need you to open and read the file.
+#
+#  
+
+#------------------------------------------------------------
+
+# SOLUTION
